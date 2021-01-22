83092313ccbbf3c4cda68591567ca1eee84a3ca2
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-22 19:16:37 +0000

    Init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="text-blue-500">Hello World</h1>
+  <span @click="toggleDarkMode">
+    toggle
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+
+  methods: {
+    toggleDarkMode () {
+      document.documentElement.classList.toggle('dark')
+    }
+  }
+})
+</script>
+
+<style>
+</style>
