10a5cd4db74df231eae76f84031605fa3f13be99
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-22 20:08:46 +0000

    Add linting

---
+++ eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-undef': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
+  }
+}
